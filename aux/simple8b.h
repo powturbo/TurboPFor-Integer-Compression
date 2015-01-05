0c04c7ace103a5db501066830208378e81ca3a8e
powturbo <powturbo@gmail.com>
2015-01-05 23:15:01 +0100

    New functions + Integer Lists Intersection

--- aux/simple8b.h
+++
@@ -1,2 +0,0 @@
-unsigned char *s8benco(unsigned      *__restrict__ in, int n, unsigned char *__restrict__ out);
-unsigned char *s8bdeco(unsigned char *__restrict__ in, int n, unsigned *__restrict__ out);
