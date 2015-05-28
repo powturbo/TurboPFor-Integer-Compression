a4f060fa7001213a770e4a4e72aa4ebbf711c516
powturbo <powturbo@gmail.com>
2015-05-28 16:50:26 +0200

    .

---
+++ transpose.h
@@ -0,0 +1,48 @@
+/**
+    Copyright (C) powturbo 2013-2015
+    GPL v2 License
+  
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    - homepage : https://sites.google.com/site/powturbo/
+    - github   : https://github.com/powturbo
+    - twitter  : https://twitter.com/powturbo
+    - email    : powturbo [_AT_] gmail [_DOT_] com
+**/
+//     transpose.h - byte transpose 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void transpose2     (unsigned char *in, unsigned n, unsigned char *out);
+void transpose3     (unsigned char *in, unsigned n, unsigned char *out);
+void transpose4     (unsigned char *in, unsigned n, unsigned char *out);
+void transpose8     (unsigned char *in, unsigned n, unsigned char *out);
+void transpose16    (unsigned char *in, unsigned n, unsigned char *out);
+void transpose      (unsigned char *in, unsigned n, unsigned char *out, unsigned esize);
+
+void untranspose2   (unsigned char *in, unsigned n, unsigned char *out);
+void untranspose3   (unsigned char *in, unsigned n, unsigned char *out);
+void untranspose4   (unsigned char *in, unsigned n, unsigned char *out);
+void untranspose8   (unsigned char *in, unsigned n, unsigned char *out);
+void untranspose16  (unsigned char *in, unsigned n, unsigned char *out);
+void untranspose    (unsigned char *in, unsigned n, unsigned char *out, unsigned esize);
+
+void transposev4  (unsigned char *in, unsigned n, unsigned char *out);
+void untransposev4(unsigned char *in, unsigned n, unsigned char *out);
+
+#ifdef __cplusplus
+}
+#endif
