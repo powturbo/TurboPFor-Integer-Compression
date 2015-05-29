679968b98878926a6a9369909678569086985a4f
powturbo <powturbo@gmail.com>
2015-05-29 17:25:46 +0200

    .

--- bitunpackv32_.h
+++ bitunpackv32_.h
@@ -1,3 +1,26 @@
+/**
+    Copyright (C) powturbo 2013-2015
+    GPL v2 License
+  
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    - homepage : https://sites.google.com/site/powturbo/
+    - github   : https://github.com/powturbo
+    - twitter  : https://twitter.com/powturbo
+    - email    : powturbo [_AT_] gmail [_DOT_] com
+**/ 
 /*#define BITUNBLKV32_0(ip, i, op, parm) {\
   OPU(op,i*0+ 0, 0, parm);\
   OPU(op,i*0+ 1, 0, parm);\
@@ -1023,4 +1046,3 @@
   BITUNBLKV32_32(ip, 30, op, parm);\
   BITUNBLKV32_32(ip, 31, op, parm);\
 }
-
