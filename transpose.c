9f53029057bdfa261475e33b0283944b04d61823
powturbo <powturbo@gmail.com>
2015-05-31 22:34:16 +0200

    .

--- transpose.c
+++ transpose.c
@@ -21,7 +21,7 @@
     - twitter  : https://twitter.com/powturbo
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
-//  transpose.c - byte transpose
+//   transpose.c - byte transpose
 
   #ifndef ESIZE
 #include "transpose.h"
