b77ae1b36e58dd9d7eb083866995c2b86bfe05ff
powturbo <powturbo@gmail.com>
2015-06-11 22:10:53 +0200

    Inverted Index

--- idx.h
+++ idx.h
@@ -50,3 +50,4 @@ typedef struct { uint8_t offseth; uint32_t offsetl; } __attribute__ ((packed)) t
 #define TIDMAPSET(__t, __ofs) { (__t)->offseth = (__ofs)>>32; (__t)->offsetl = (__ofs) & 0xffffffff; }
 #define TIDMAPGET(__t) ((__off64_t)(__t)->offseth << 32 | (__t)->offsetl)
 #define TIDMAP(__fdm, __tid) ({ unsigned char *_bp = __fdm; tmap_t *_t = (tmap_t *)&_bp[(__tid)*sizeof(tmap_t)]; TIDMAPGET(_t); })
+
