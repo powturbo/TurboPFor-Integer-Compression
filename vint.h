5fba6eb717e98606ea5e38040856302f7c094b3f
powturbo <powturbo@gmail.com>
2015-06-11 12:07:56 +0200

    Variable Byte

--- vint.h
+++ vint.h
@@ -25,6 +25,7 @@
 
 #ifndef VINT_H
 #define VINT_H
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -40,6 +41,30 @@ extern "C" {
   else if(likely(__x < (1<<28))) { *(unsigned       *)__op = __x << 4 | 0x07; __op += 4; 		              __act;}\
   else { 		                   *(unsigned       *)__op = __x << 4 | 0x0f; __op += 4; *__op++ = __x >> 28; __act;}\
 }
+
+#define vbputa64(__op, __x, __act) {\
+       if(__x < 1   << 7) {		   		        *__op++ = __x << 1; 			 		                                                                   __act;}\
+  else if(__x < 1   <<14) { *(unsigned short     *)__op = __x << 2 | 0x01; __op += 2; 		             	                                               __act;}\
+  else if(__x < 1   <<21) { *(unsigned short     *)__op = __x << 3 | 0x03; __op += 2;                 *__op++ = __x >> 13;                                 __act;}\
+  else if(__x < 1   <<28) { *(unsigned           *)__op = __x << 4 | 0x07; __op += 4; 		                                                               __act;}\
+  else if(__x < 1ull<<35) { *(unsigned           *)__op = __x << 5 | 0x0f; __op += 4;                 *__op++ = __x >> 27;                                 __act;}\
+  else if(__x < 1ull<<42) { *(unsigned           *)__op = __x << 6 | 0x1f; __op += 4; *(unsigned short *)__op = __x >> 26;                        __op+=2; __act;}\
+  else if(__x < 1ull<<49) { *(unsigned           *)__op = __x << 7 | 0x3f; __op += 4; *(unsigned short *)__op = __x >> 25; *(__op+2) = __x >> 41; __op+=3; __act;}\
+  else if(__x < 1ull<<56) { *(unsigned long long *)__op = __x << 8 | 0x7f; __op += 8;                                                                      __act;}\
+  else {                                          *__op++ =          0xff;        *(unsigned long long *)__op = __x;                              __op+=8; __act;}\
+}
+
+#define vbgeta64(__ip, __x, __act) do {\
+  if(!((__x = *__ip) & 1<<0)) {  __ip++; __x	                    >>= 1; 		                                                                __act;}\
+  else if(!(__x      & 1<<1)) { __x = (*(unsigned short     *)__ip) >>  2;		                                                     __ip += 2; __act;}\
+  else if(!(__x      & 1<<2)) { __x = (*(unsigned short     *)__ip) >>  3 | *(__ip+2) << 13;                                         __ip += 3; __act;}\
+  else if(!(__x      & 1<<3)) { __x = (*(unsigned           *)__ip) >>  4; 		      	                                             __ip += 4; __act;}\
+  else if(!(__x      & 1<<4)) { __x = (*(unsigned           *)__ip) >>  5 | (unsigned long long)(*(unsigned char  *)(__ip+4)) << 27; __ip += 5; __act;}\
+  else if(!(__x      & 1<<5)) { __x = (*(unsigned           *)__ip) >>  6 | (unsigned long long)(*(unsigned short *)(__ip+4)) << 26; __ip += 6; __act;}\
+  else if(!(__x      & 1<<6)) { __x = (*(unsigned           *)__ip) >>  7 | (unsigned long long)(*(unsigned short *)(__ip+4)) << 25 | (unsigned long long)(*(__ip+6)) << 41; __ip += 7; __act;}\
+  else if(!(__x      & 1<<7)) { __x = (*(unsigned long long *)__ip) >>  8; 		                                                     __ip += 8; __act;}\
+  else 			   	          { __x =  *(unsigned long long *)(__ip+1);                                                              __ip += 9; __act;}\
+} while(0)
 
 //#define __AVX2__VINT
   
@@ -78,29 +103,37 @@ extern unsigned char vtab[];
 // encode/decode single value
 #define vbput(__op, __x) { unsigned _x_ = __x; vbputa(__op, _x_, ;); }
 #define vbget(__ip) ({     unsigned _x_;       vbgeta(__ip, _x_, ;); _x_; })
+#define vbput32(__op, __x) vbput(__op, __x)
+#define vbget32(__ip) vbget(__ip)
+
+#define vbput64(__op, __x) { unsigned long long _x_ = __x; vbputa64(__op, _x_, ;); }
+#define vbget64(__ip) ({     unsigned long long _x_;       vbgeta64(__ip, _x_, ;); _x_; })
 
 // encode array with n unsigned (32 bits in[n]) values to the buffer out. Return value = end of compressed buffer out
 unsigned char *vbenc   (unsigned      *__restrict in, unsigned n, 				  unsigned char  *__restrict out);
+unsigned char *vbenc16(unsigned short *__restrict in, unsigned n, 				  unsigned char  *__restrict out);
+unsigned char *vbenc64(uint64_t       *__restrict in, unsigned n, 			      unsigned char  *__restrict out);
+#define vbenc32(in,n,out) vbenc(in,n,out)
+#define vbdec32(in,n,out) vbdec(in,n,out)
 
 // decompress buffer into an array of n unsigned values. Return value = end of decompressed buffer in
 unsigned char *vbdec   (unsigned char *__restrict in, unsigned n, 				  unsigned       *__restrict out);
+unsigned char *vbdec16(unsigned char  *__restrict in, unsigned n, 				  unsigned short *__restrict out);
+unsigned char *vbdec64(unsigned char  *__restrict in, unsigned n, 			      uint64_t       *__restrict out);
 //---------------------------------- increasing integer lists -------------------------------------------------------------
 unsigned char *vbdenc  (unsigned      *__restrict in, unsigned n, unsigned char  *__restrict out, unsigned start);
 unsigned char *vbddec  (unsigned char *__restrict in, unsigned n, unsigned       *__restrict out, unsigned start);
 
-//---------------------------------- strictly increasing (never remaining constant or decreasing) integer lists-------------
+//---------------------------------- strictly increasing (never remaining constant or decreasing) integer lists------------
 unsigned char *vbd1enc (unsigned      *__restrict in, unsigned n, unsigned char  *__restrict out, unsigned start);
 unsigned char *vbd1dec (unsigned char *__restrict in, unsigned n, unsigned       *__restrict out, unsigned start);
 
-//---------------------------------- variable byte : 15 bits (like vbenc/vbdec but for 16 bits integer lists) --------
+//---------------------------------- variable byte : 15 bits integer lists ------------------------------------------------
 #define vbput16(__op, __x) do { unsigned _x = __x; if(likely(_x < 0x80)) *__op++ = _x; else { *__op++ = (_x) >> 8 | 0x80; *__op++ = _x; } } while(0)
 #define vbgeta16(__ip,__x, __act) do { if((__x = *__ip++) > 0x7f) __x = (__x & 0x7f) << 8 | *__ip++; __act; } while(0)
 
 #define vblen16(__x) ((__x) > 0x7f?2:1)
 #define vbget16(__ip) ({ unsigned _x; vbgeta16(__ip, _x, ;); _x; })
-
-unsigned char *vbenc16(unsigned short *__restrict in, unsigned n, 				  unsigned char  *__restrict out);
-unsigned char *vbdec16(unsigned char  *__restrict in, unsigned n, 				  unsigned short *__restrict out);
 
 #ifdef __cplusplus
 }
