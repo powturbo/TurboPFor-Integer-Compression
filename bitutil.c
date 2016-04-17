fc221ec70f923db79f3d42403e38d48ef1b5e801
powturbo <powturbo@gmail.com>
2016-04-17 16:02:13 +0200

    BitUtil: Delta, ZigZag, NumBits, Floating Point,...

--- bitutil.c
+++ bitutil.c
@@ -21,7 +21,7 @@
     - twitter  : https://twitter.com/powturbo
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
-//   bitutil.h - "Integer Compression" 
+//  bitutil.h - "Integer Compression" 
 #include "conf.h" 
 #include "bitutil.h"
 
