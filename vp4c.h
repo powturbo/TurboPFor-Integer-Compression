2e0eea1e4ef5455a398621c5460f4946adf01605
x <powturbo@gmail.com>
2017-01-04 15:03:23 +0100

    TurboPFor: TurboPFor encode header

--- vp4c.h
+++ vp4c.h
@@ -21,7 +21,7 @@
     - twitter  : https://twitter.com/powturbo
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
-//     vp4dc.h - "Integer Compression" TurboPfor  (see vp4dd.h for decompression) 
+//  "Integer Compression" TurboPfor  (see vp4dd.h for decompression) 
  
 #define P4EB(_b_) (_b_ << 1)
 #define P4EBX(_b_, _bx_) (_bx_ << 8 | _b_ << 1 | 1) 
