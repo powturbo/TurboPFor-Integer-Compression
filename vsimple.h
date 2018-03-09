b2414ced7160ae09a665ef2ef4ef8a9deb7be62e
x <powturbo@gmail.com>
2018-03-09 17:57:52 +0100

    TurboPFor: Variable simple header

--- vsimple.h
+++ vsimple.h
@@ -31,14 +31,16 @@ extern "C" {
 #endif
 
 // vsencNN: compress array with n unsigned (NN bits in[n]) values to the buffer out. Return value = end of compressed output buffer out
-unsigned char *vsenc16(unsigned short *__restrict in, int n, unsigned char      *__restrict out);
-unsigned char *vsenc32(unsigned       *__restrict in, int n, unsigned char      *__restrict out);
-unsigned char *vsenc64(uint64_t       *__restrict in, int n, unsigned char      *__restrict out);
+unsigned char *vsenc8( unsigned char  *__restrict in, size_t n, unsigned char  *__restrict out);
+unsigned char *vsenc16(unsigned short *__restrict in, size_t n, unsigned char  *__restrict out);
+unsigned char *vsenc32(unsigned       *__restrict in, size_t n, unsigned char  *__restrict out);
+unsigned char *vsenc64(uint64_t       *__restrict in, size_t n, unsigned char  *__restrict out);
 
 // vsdecNN: decompress buffer into an array of n unsigned values. Return value = end of compressed input buffer in 
-unsigned char *vsdec16(unsigned char  *__restrict in, int n, unsigned short     *__restrict out);
-unsigned char *vsdec32(unsigned char  *__restrict in, int n, unsigned           *__restrict out);
-unsigned char *vsdec64(unsigned char  *__restrict in, int n, uint64_t           *__restrict out);
+unsigned char *vsdec8( unsigned char  *__restrict in, size_t n, unsigned char  *__restrict out);
+unsigned char *vsdec16(unsigned char  *__restrict in, size_t n, unsigned short *__restrict out);
+unsigned char *vsdec32(unsigned char  *__restrict in, size_t n, unsigned       *__restrict out);
+unsigned char *vsdec64(unsigned char  *__restrict in, size_t n, uint64_t       *__restrict out);
 
 #ifdef __cplusplus
 }
