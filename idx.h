590da7331d68e8a85831d077797f31e9623e9f81
x <powturbo@gmail.com>
2019-12-21 15:19:29 +0100

    Inverted index: header for idxcr/idxqry

--- idx.h
+++ idx.h
@@ -1,7 +1,7 @@
 /**
     Copyright (C) powturbo 2013-2019
     GPL v2 License
-  
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -21,28 +21,28 @@
     - twitter  : https://twitter.com/powturbo
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
-    
-//	  "Integer Compression" header for idxcr/idxqry
+
+//    "Integer Compression" header for idxcr/idxqry
 #include <stdint.h>
 
-#define BLK_DIDNUM (128+1) 	//  Block size 128 + 1 (1 stored in skips)
+#define BLK_DIDNUM (128+1)  //  Block size 128 + 1 (1 stored in skips)
 
-							//				compressed size for 62 GB clueweb09.sorted
-   					   		// Defaut is bitpackv/bitunpackv    18 GB
-//#define _TURBOPFOR     	// for compact version     			12 GB  
+                            //              compressed size for 62 GB clueweb09.sorted
+                            // Defaut is bitpackv/bitunpackv    18 GB
+//#define _TURBOPFOR        // for compact version              12 GB
 
   #ifdef _TURBOPFOR
-//#define SKIP_S 6 
-#define SKIP_SIZE 2  		// always no implicit skip. 
+//#define SKIP_S 6
+#define SKIP_SIZE 2         // always no implicit skip.
   #else
 //#define SKIP_S 5
-#define SKIP_SIZE 2  	    // no implicit skips 
-//#define SKIP_SIZE 1  	    //    implicit skips
+#define SKIP_SIZE 2         // no implicit skips
+//#define SKIP_SIZE 1       //    implicit skips
   #endif
 #define SKIP_M ((1<<SKIP_S)-1)
 
   #ifdef _WIN32
-#define __off64_t _off64_t  
+#define __off64_t _off64_t
   #endif
 //-------------------------- Mapping term id <-> posting offset in file ----------------------------------
 typedef struct { uint8_t offseth; uint32_t offsetl; } __attribute__ ((packed)) tmap_t;   // 40 bits offsets -> 1 Terabyte
