3f420ba3c323f0164b0e3a11433da73cbf7aa936
x <powturbo@gmail.com>
2019-12-21 15:19:30 +0100

    TurboPFor: Variable simple header

--- vsimple.h
+++ vsimple.h
@@ -1,7 +1,7 @@
 /**
     Copyright (C) powturbo 2013-2019
     GPL v2 License
-  
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -22,9 +22,9 @@
     - email    : powturbo [_AT_] gmail [_DOT_] com
 **/
 //  "Integer Compression" variable simple "SimpleV"
-//	this belongs to the integer compression known as "simple family", like simple-9,simple-16
-//	or simple-8b. SimpleV is compressing integers in groups into variable word size 32, 40 and 64 bits + RLE (run length encoding) 
-//	SimpleV is faster than simple-16 and compress better than simple-16 or simple-8b.
+//  this belongs to the integer compression known as "simple family", like simple-9,simple-16
+//  or simple-8b. SimpleV is compressing integers in groups into variable word size 32, 40 and 64 bits + RLE (run length encoding)
+//  SimpleV is faster than simple-16 and compress better than simple-16 or simple-8b.
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,7 +36,7 @@ unsigned char *vsenc16(unsigned short *__restrict in, size_t n, unsigned char  *
 unsigned char *vsenc32(unsigned       *__restrict in, size_t n, unsigned char  *__restrict out);
 unsigned char *vsenc64(uint64_t       *__restrict in, size_t n, unsigned char  *__restrict out);
 
-// vsdecNN: decompress buffer into an array of n unsigned values. Return value = end of compressed input buffer in 
+// vsdecNN: decompress buffer into an array of n unsigned values. Return value = end of compressed input buffer in
 unsigned char *vsdec8( unsigned char  *__restrict in, size_t n, unsigned char  *__restrict out);
 unsigned char *vsdec16(unsigned char  *__restrict in, size_t n, unsigned short *__restrict out);
 unsigned char *vsdec32(unsigned char  *__restrict in, size_t n, unsigned       *__restrict out);
