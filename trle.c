efa4fa55b8cbaae237a794bff7a0b428a5993922
x <powturbo@gmail.com>
2023-03-17 20:06:53 +0100

    TurboRLE: Benchmark App

--- trle.c
+++ trle.c
@@ -1,6 +1,6 @@
 /**
-    Copyright (C) powturbo 2015-2018
-    GPL v2 License
+    Copyright (C) powturbo 2015-2023
+    SPDX-License-Identifier: GPL v2 License
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
