179c7324c917d9ac70f16396c125f16511e77122
x <powturbo@gmail.com>
2023-03-31 18:13:37 +0200

    Rust binding

---
+++ rust/src/lib.rs
@@ -0,0 +1,9 @@
+// prerequisites: install TurboPFor library libic  
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+//include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+include!("bindings.rs");
+
+#[cfg(test)]
+mod tests;
