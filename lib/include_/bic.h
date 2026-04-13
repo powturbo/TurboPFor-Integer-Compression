f233fce56cf040599a6d8c374bd8f0a9acaaceb7
powturbo <powturbo@gmail.com>
2026-04-13 19:55:54 +0200

    TurboPFor: Binary Interpolative Coding

--- lib/include_/bic.h
+++ lib/include_/bic.h
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) powturbo 2013-2023
+    Copyright (C) powturbo 2013-2026
     SPDX-License-Identifier: GPL v2 License
 
     This program is free software; you can redistribute it and/or modify
